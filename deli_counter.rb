bd52c173300e434e012dd862e2bd4f8e90d28176
Oscar Ore <oscar.ore2011@gmail.com>
2020-01-31 03:45:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
+require 'pry'
+
 # Write your code here.
+
+
+
+
+
+
+
+def line(katz_deli)
+  if katz_deli.empty?
+   puts "The line is currently empty."
+ else 
+   katz_deli.each do 
+  end
+end 
+
+
+#"The line is currently: 1. Logan 2. Avi 3. Spencer"

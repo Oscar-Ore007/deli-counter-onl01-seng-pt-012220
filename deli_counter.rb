64a6875cd477f4621ee87015194c198dbb705fe5
Oscar Ore <oscar.ore2011@gmail.com>
2020-01-31 03:52:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(katz_deli)
   if katz_deli.empty?
    puts "The line is currently empty."
  else 
-   current_sentence = "The line is currently "
+   current_sentence = "The line is currently:"
    counter = 1 
    katz_deli.each do |p|
      puts "#{counter}. #{p}"
